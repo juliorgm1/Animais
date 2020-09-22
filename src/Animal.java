222a74cbf14b1f4b5f70cf11d91eb0dc6c4778cc
juliorgm1 <julio.developer1@gmail.com>
2020-09-22 15:29:53 -0300

    Adição do construtor da classe Animal

--- src/Animal.java
+++ src/Animal.java
@@ -1,4 +1,9 @@
 public class Animal {
     private String nome;
     private String idade;
+
+    public Animal(String nome, String idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
 }
